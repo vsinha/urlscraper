8db7d56a2e5308e0c7293e4ca66af42e113b996d
Viraj Sinha <virajosinha@gmail.com>
2015-10-30 00:21:13 -0400

    updating

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var formidable = require('formidable');
 var google = require('google');
 
 google.resultsPerPage = 10
-var nextCounter = 0
 
 var server = http.createServer(function (req, res) {
   if (req.method.toLowerCase() == 'get') {
@@ -28,33 +27,35 @@ function displayForm(res) {
 
 function processAllFieldsOfTheForm(req, res) {
   var form = new formidable.IncomingForm();
+  var nextCounter = 0;
 
   console.log('processing form');
 
   form.parse(req, function (err, fields, files) {
-
     console.log("received form query: " + fields['query']);
 
     google(fields['query'], function(error, next, links) {
     
       if (error) console.error(error)
-
-      //Store the data from the fields in your data store.
-      //The data store could be a file or database or any other store based
-      //on your application.
+      
       res.writeHead(200, {
         'content-type': 'text/plain'
       });
 
       for (var i = 0; i < links.length; ++i) {
         var urlstring = links[i].link;
-        console.log(urlstring);
-        res.write(urlstring);
-        res.write('\n');
+
+        if (urlstring) {
+          console.log(links[i].title + ' - ' + links[i].link) // link.href is an alias for link.link
+          console.log(links[i].description + "\n")
+          console.log(urlstring);
+          res.write(urlstring);
+          res.write('\n');
+          nextCounter += 1;
+        }
       }
 
-      if (nextCounter < 4) {
-        nextCounter += 1
+      if (nextCounter < 10) {
         if (next) next()
       }
 
